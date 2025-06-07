3dc7763870db5f2daf2bf1d3fbc13508366fc39a
Szymon Ligenz <szymek.ligenza@gmail.com>
2025-06-07 19:38:14 +0200

    added healthcheck and status colors

--- frontend/src/components/ParcelList.jsx
+++ frontend/src/components/ParcelList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { getParcel } from '../api'
+import './ParcelList.css'
 
 export default function ParcelList({ trackingNumbers }) {
   const [parcels, setParcels] = useState([])
@@ -9,15 +10,19 @@ export default function ParcelList({ trackingNumbers }) {
       Promise.all(trackingNumbers.map(getParcel))
         .then(setParcels)
         .catch(console.error)
-    }, 1000)
+    }, 100)
     return () => clearInterval(interval)
   }, [trackingNumbers])
+
+  const statusClass = status =>
+    'status-' + status.replace(/\s/g, '').toLowerCase()
 
   return (
     <ul>
       {parcels.map(p => (
         <li key={p.tracking_number}>
-          <strong>{p.tracking_number}</strong> - {p.status}
+          <strong>{p.tracking_number}</strong> -{' '}
+          <span className={statusClass(p.status)}>{p.status}</span>
         </li>
       ))}
     </ul>
